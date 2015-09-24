2d8e43aba96052781e9a193b3c9c3197c451dbbe
artorr94 <artorr94@gmail.com>
2015-09-24 13:07:05 +0300

    zaliv proekta

---
+++ app/src/androidTest/java/com/artorr94/launcherartofjeam/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.artorr94.launcherartofjeam;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
